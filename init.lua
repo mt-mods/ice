beb56cb6c85d9d9624d080045cb4a367bb8e4c72
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-05-30 20:25:12 -0500

    Add support for translations
    Translations were translated with AI.
    All code was done with a human hand, translation files were generated with `xgettext` and `msginit` commands.

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+local S, PS = core.get_translator("ice")
 
 -- Ice Brick
 
-minetest.register_node("ice:brick", {
-	description = "Ice Brick",
+core.register_node("ice:brick", {
+	description = S("Ice Brick"),
 	tiles = {
 		"ice_brick_tb.png",
 		"ice_brick_tb.png",
@@ -15,7 +16,7 @@ minetest.register_node("ice:brick", {
 	sounds = default.node_sound_ice_defaults(),
 })
 
-minetest.register_craft{
+core.register_craft{
 	output = "ice:brick",
 	recipe = {
 		{"default:ice", "default:ice"},
@@ -28,14 +29,14 @@ minetest.register_craft{
 
 walls.register(
 	"ice:wall",
-	"Ice Brick Wall",
+	S("Ice Brick Wall"),
 	"ice_brick_sides.png",
 	"ice:brick",
 	default.node_sound_ice_defaults()
 )
 
 -- Fix groups (ice is not stone)
-minetest.override_item("ice:wall", {
+core.override_item("ice:wall", {
 	groups = {cracky = 3, cools_lava = 1, slippery = 3, wall = 1}
 })
 
@@ -44,7 +45,7 @@ minetest.override_item("ice:wall", {
 
 doors.register("ice:door", {
 	tiles = {"ice_door.png"},
-	description = "Ice Door",
+	description = S("Ice Door"),
 	inventory_image = "ice_door_inv.png",
 	groups = {cracky = 3, cools_lava = 1, slippery = 3},
 	sounds = default.node_sound_ice_defaults(),
